ffdb607052bfbaa2f7cfda02961fbd0b3729092b
ersincebi <ersincebi.1995@gmail.com>
2020-06-07 23:15:45 +0300

    tests are finished, ODD file init, presentation file init

--- tests/resultPlotting.py
+++ tests/resultPlotting.py
@@ -2,47 +2,102 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-basePath = "./results"
+import scipy.stats as stats
+import pylab as pl
+from scipy.integrate import trapz
+from scipy.stats import ttest_ind
 
+basePath = "./results"
+data={}
+loss = {}
 def joinPath(arr):
 	return '/'.join(arr)
 
 def returnList(dir):
 	return np.array(os.listdir(dir))
 
-def plot(data):
-	bins = range(len(data[0]))
-	ranges = (data.min(),data.max())
-	plt.hist(data[0],bins,ranges)
-	plt.hist(data[1],bins,ranges)
-	plt.legend(['dense','focused'])
-	plt.show()
+def lossComparison(name, results):
+	plt.plot(results, label=name+" loss")
+	plt.ylabel('loss')
+	plt.xlabel('epoch')
+	plt.legend(loc="upper left")
+	
+def showTrend(scores, name=''):
+	x = []
+	y = []
+	for i in range(0, len(scores)):
+		x.append(int(i))
+		y.append(int(scores[i]))
+
+	plt.plot(x, y, label=name+" score per run")
+
+	trend_x = x[1:]
+	z = np.polyfit(np.array(trend_x), np.array(y[1:]), 1)
+	p = np.poly1d(z)
+	plt.plot(trend_x, p(trend_x), linestyle="-.", label=name+" trend")
 
 
-for folder in returnList(basePath)[[1]]:
-	dir = joinPath([basePath, folder])
-	data={}
+	plt.xlabel("episodes")
+	plt.ylabel("scores")
+	plt.legend(loc="upper left")
+
+def plot(folder):
+	dir = joinPath([basePath, folder[0]])
 	f = ''
 	for file in returnList(dir):
+
 		selectFolder = joinPath([dir,file])
 		selectFile = returnList(selectFolder)[[3]]
 		f = joinPath([selectFolder,selectFile[0]])
+
+		selectLossFile = returnList(selectFolder)[[1]]
+		l = joinPath([selectFolder,selectLossFile[0]])
+
 		data[file] = np.load(f)
+		loss[file] = np.load(l)
 
-print(np.array(data))
+		d = np.sort(data[file])
+	# 	print(f'Area Under {file} Curve',trapz(d))
+
+	# 	fit = stats.norm.pdf(d, np.mean(d), np.std(d))
+	# 	pl.plot(d, fit,'-o')
+	# 	pl.hist(d, density=True)
+
+	# pl.legend(data.keys())
+	# plt.savefig('./plots/histograms/'+folder[0]+'_histogram.png', bbox_inches="tight")
 
 
-# normal distro
-# import numpy as np
-# import scipy.stats as stats
-# import pylab as pl
+# histogram display over score table on a problem
+selectedFolder = returnList(basePath)[[0]]
+# pl.figure(figsize=(20,15))
+plot(selectedFolder)
+# pl.show()
 
-# h = sorted([186, 176, 158, 180, 186, 168, 168, 164, 178, 170, 189, 195, 172]) #sorted
+#This part will run after selecting right focused values againts dense values
+# trend comparison
+selectedDenseScore = data['dense']
+selectedFocusedScore = data['focused-02']
+selectedDenseLoss = loss['dense']
+selectedFocusedLoss = loss['focused-02']
+plt.figure(figsize=(20, 15))
+showTrend(name=selectedFolder[0]+' dense',scores=selectedDenseScore)
+showTrend(name=selectedFolder[0]+' focused',scores=selectedFocusedScore)
+plt.savefig('./plots/histograms/'+selectedFolder[0]+'_trend.png', bbox_inches="tight")
+plt.show()
 
-# fit = stats.norm.pdf(h, np.mean(h), np.std(h))  #this is a fitting indeed
+# # loss comparison
+plt.figure(figsize=(20, 15))
+lossComparison(name=selectedFolder[0]+' dense',results=selectedDenseLoss)
+lossComparison(name=selectedFolder[0]+' focused',results=selectedFocusedLoss)
+print(f'Area Under dense Curve',trapz(selectedDenseLoss))
+print(f'Area Under focused Curve',trapz(selectedFocusedLoss))
+plt.savefig('./plots/histograms/'+selectedFolder[0]+'_loss.png', bbox_inches="tight")
+plt.show()
 
-# pl.plot(h,fit,'-o')
-
-# pl.hist(h,normed=True)      #use this to draw histogram of your data
-
-# pl.show()                   #use may also need add this
+# t-test will applied according to our hypotesis
+# 	we will make an assumption according to area under curve
+# 	and will say like this focused nn learn to solve
+# 	the problem more fast, accurate and efficient
+statistic, pvalue = ttest_ind(selectedDenseScore,selectedFocusedScore)
+print('statistic: ',float(statistic))
+print('pvalue: ',float(pvalue))
